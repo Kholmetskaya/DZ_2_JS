31666fc4f68f233fde0c8bc14620f8d206860d90
Jane <Bo4koJane@i.ua>
2018-03-11 17:32:32 +0200

    Change zadanie_7

--- zadanie_7.js
+++ zadanie_7.js
@@ -3,12 +3,12 @@
 // Пользователь вводит 1 числа
 // Найти в массиве пару чисел которые в сумме бы дали это число
 
-var num = +process.argv[2] ,
-array = [1, 6, 45, 2, 7, 5, -45, 4, 3];
+var num = +process.argv[2] , 
+array = [1, 3, 7, 6, 9, 45, -45, 4, 8];
 
 for (var i = 0; i < array.length; i++) {
 	for ( var j = 0; j < array.length; j++) {
-		if (array[i] + array[j] == num && array[i] != array[j]) {			
+		if (array[i] + array[j] == num && array[j] > array[i]) {			
 			console.log(`Ваша пара чисел это:  ${array[i]}  и   ${array[j]} `);
         }  
     }
